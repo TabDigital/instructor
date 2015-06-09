f9db343eb64f56b93366ef1ac32f19bad9507665
Julio Cesar Ody <julio@awesomebydesign.com>
2015-06-09 14:15:47 +1000

    Rendering overlay to body. Base styles

--- src/components/instructor/index.js
+++ src/components/instructor/index.js
@@ -6,6 +6,18 @@
 // ./mock-instructions.js.
 const selector = '[data-instructable]'
 
+const overlayTemplate = function(attributes = {}) {
+  return `
+    <div class="instructable-wrapper">
+      <h1 class="heading">${attributes.title}</h1>
+      <div class="body-wrapper">${attributes.body}</div>
+    </div>
+  `.trim()
+}
+
+const overlayElement = document.createElement('div')
+overlayElement.classList.add('instructable-overlay')
+
 export default class Instructor {
   constructor(rootEl, options = {}) {
     this.rootEl = rootEl
@@ -16,5 +28,21 @@ export default class Instructor {
   findAllInstructables() {
     let elements = this.rootEl.querySelectorAll(selector)
     return [].slice.apply(elements)
+  }
+
+  show(key) {
+    let instructions = this.instructions[key]
+    if (!instructions) {
+      throw new Error(`Instructor: No instructions found for ${key}`)
+    }
+    if (!overlayElement.parentNode) this.rootEl.appendChild(overlayElement)
+    overlayElement.innerHTML = overlayTemplate(instructions)
+    overlayElement.classList.add('visible')
+  }
+
+  removeOverlay() {
+    if (overlayElement.parentNode) {
+      overlayElement.parentNode.removeChild(overlayElement)
+    }
   }
 }
--- src/components/instructor/mock-instructions.js
+++ src/components/instructor/mock-instructions.js
@@ -4,6 +4,10 @@
 // This gives an example of how the instructions object should look like.
 
 export default {
+  'green-box': {
+    title: 'Look, a green box',
+    body: 'An ok green box'
+  },
   'blue-box': {
     title: 'Look, a blue box',
     body: 'A very, very nice blue box'
